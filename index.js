86964f9341a1145a9e19ea9cc6b5a9587a17fdb1
KseniaSafonova <kseniatsn@gmail.com>
2022-10-30 16:56:48 +0300

    .

--- index.js
+++ index.js
@@ -1,15 +1,35 @@
 
-const lottery = async () => {
 
+function lottery() {
+    console.log("Вы начали игру");
     let promise = new Promise(function (resolve, reject) {
         setTimeout(function () {
-            Math.random(0) > 0.5 ? resolve('Вы выиграли' + Math.random(0)) : reject('Вы проиграли' + Math.random(0))
-        }, 1000)
-    })
-
-    let result = await promise;
-    console.log(result)
-
+            Math.random(0) > 0.5 ? resolve() : reject("Вы промахнулись");
+        }, 1000);
+    });
+    return promise;
 }
 
 lottery()
+    .then(() => console.log("Вы выиграли"))
+    .then(() => console.log("Вам заплатили"))
+    .catch(() => console.log("Вы проиграли"))
+    .finally(() => console.log("Игра закончена"));
+
+
+
+// const lottery = async () => {
+
+//     let promise = new Promise(function (resolve, reject) {
+//         setTimeout(function () {
+//             Math.random(0) > 0.5 ? resolve('Вы выиграли ' + Math.random(0)) : reject('Вы проиграли ' + Math.random(0))
+//         }, 5000)
+//     })
+
+//     let result = await promise;
+//     console.log(result)
+// };
+
+// lottery()
+//     // .then((res) => console.log(res))
+//     // .then((err) => console.log(err))
